9f34cc448a04a49a20c99f3690a98ab1848bc88e
Franco León Cáceres <francocaceres008@gmail.com>
2023-10-10 16:18:06 -0300

    feat(client/views/landingPage):added LandingPage styles

--- client/src/views/landingPage/LandingPage.tsx
+++ client/src/views/landingPage/LandingPage.tsx
@@ -4,7 +4,14 @@ import styles from "./LandingPage.module.scss";
 function LandingPage() {
   return (
     <main className={styles.mainLanding}>
-      <h1 className={styles.title}>IQNet</h1>
+      <section className={styles.sectionLanding}>
+        <h1 className={styles.title}>IQNet</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sed
+          voluptatum dolorem, ab facere temporibus qui veniam non repellendus
+          quae.
+        </p>
+      </section>
     </main>
   );
 }
